82f88483c139bda0c8fefb24440859f100e0a88b
Caspian Rosengren <caspian@rosengren.nu>
2022-05-01 16:07:57 +0200

    La till att den bler enklare att endra variablar

--- tes navig.py
+++ tes navig.py
@@ -2,7 +2,7 @@ import random
 from time import sleep
 from tkinter import NE
 import pygame
-
+import multiprocessing
 
 
 from numpy import var
@@ -35,42 +35,87 @@ def TillbackaVextning(Xa, Ya, TilpacaVext, MaxNertramp):
             #if Gride[Xt][Yt] > MaxNertramp:
             #    Gride[Xt][Yt] = MaxNertramp
             if Gride[Xt][Yt] > 1:
-                Gride[Xt][Yt] = Gride[Xt][Yt] / TilpacaVext 
+                Gride[Xt][Yt] = Gride[Xt][Yt] - TilpacaVext 
             Yt += 1
         Xt += 1
 
-def SjärmUpdaterare(Xa, Ya, pixAr, GoalX, GoalY):
+def SjärmUpdaterare(Xa, Ya, pixAr, GoalX, GoalY,pixArP,VisaKArta,VisaVargeVäg):
+    gameDisplay.fill(Black)
     Xt = 0
     while Xt < Xa:
         Yt = 0
         while Yt < Ya: 
-            F = (Gride[Xt][Yt]) * 10
+            F = (Gride[Xt][Yt]) * 100
             F = clamp(F, 0, 254)
             Ferg = (F, F, F)
-            if Gride[Xt][Yt] > 0: 
+            if Gride[Xt][Yt] > 1 and VisaKArta == 1: 
                 pixAr[Xt][Yt] = Ferg
-            elif Xt == GoalX and Yt == GoalY:
+                print("tatta")
+            elif Xt == GoalX and Yt == GoalY and VisaKArta == 1:
                 pixAr[Xt][Yt] = (250,0,0)
+
+            if PGride[Xt][Yt] > 1 and VisaVargeVäg == 1: 
+                pixArP[Xt][Yt] = (255,255,255)
+
             Yt += 1
         Xt += 1
     pygame.display.update()
+    
 
 #Överför den privata vägen till den glubala
 def Hitarät(Xa, Ya,rep , PGride):
+    
     Xt = 0
     while Xt < Xa:
         Yt = 0
         while Yt < Ya: 
             #Överför alla ruter till det glubala nätet  
-            Gride[Xt][Yt] += (PGride[Xt][Yt])/ ((rep+1)/2)
+            Gride[Xt][Yt] += (PGride[Xt][Yt])/ (rep+1)
             Yt += 1
         Xt += 1
+    PGride = [[1 for i in range(Ya)]for j in range(Xa)]
 
 
 
 from array import *
+#spelyta och start och slut kordinater 
 Xa = 150
 Ya = 300
+#start och slut kordinater 
+#[StartX, StartY, SlufX, SlutY]
+StartOchSlutStelen = [[6, 8, 70, 150], [6, 250, 70, 150], [130, 8, 70, 150], [130, 250, 6, 8]]
+
+
+
+#Variablar som kan endras
+GoalMod = 15
+    #hur mycket den trampar ner 
+Nertramp = 2
+    #tilbackavexten efter varje gång
+TilpacaVext = 1.5
+    #maximala slumptalet
+SlumpMax = 60
+    #maximala antalet nertramp
+MaxNertramp = 10000000
+    #hur många steg myrena kan ta
+MaxF = 900
+    #hur många gånger den testar en väg
+Försök = 1000
+
+#Gui instelningar 
+    #skriver ut försök
+FörsökUt = 1
+    #visar varje väg myran tar 
+VisaVargeVäg = 0
+    #visar Gride 
+VisaKArta = 0
+    #printar vilket försök den är på
+PintFörsök = 0
+
+
+
+
+#variablar som inte ska röras 
 #max x och y
 MXaxel = Xa - 1 
 MYaxel = Ya - 1
@@ -79,30 +124,26 @@ Y = 1
 rep = 0
 GoalX = 0
 GoalY = 0
-GoalMod = 35
-Nertramp = 5
-rep = 0
 Black = (0,0,0)
-TilpacaVext = 1.01
-SlumpMax = 40
-MaxNertramp = 10000000
-MaxF = 700
-Försök = 1000
-#[StartX, StartY, SlufX, SlutY]
-StartOchSlutStelen = [[6, 8, 70, 150], [6, 250, 70, 150], [130, 8, 70, 150], [130, 250, 6, 8]]
-#glubal Grid 
+#Rutnäten 
+    #glubal Grid 
 Gride = [[1 for i in range(Ya)]for j in range(Xa)] 
-#personlig grid
+    #personlig grid
 PGride = [[1 for i in range(Ya)]for j in range(Xa)] 
+
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((Xa, Ya))
 gameDisplay.fill(Black)
 pixAr = pygame.PixelArray(gameDisplay)
+#ektra sjärm
+gameDisplayP = pygame.display.set_mode((Xa, Ya))
+gameDisplayP.fill(Black)
+pixArP = pygame.PixelArray(gameDisplayP)
 #för att updatter
 
 
-def new_func(Gride, X, Y, GoalX, GoalY,rep, MaxF,PGride, Xa, Ya):
+def new_func(Gride, X, Y, GoalX, GoalY, rep, MaxF,PGride, Xa, Ya, FörsökUt):
     rep = 0
     while ((X != GoalX) or (Y != GoalY)) and rep < MaxF:
     #genererar vilket värde sam den ska till 
@@ -146,31 +187,34 @@ def new_func(Gride, X, Y, GoalX, GoalY,rep, MaxF,PGride, Xa, Ya):
         X = clamp(X, 1, MXaxel-1)
         Y = clamp(Y, 1, MYaxel-1)
         rep += 1
-    if rep == MaxF:
-        print("[X]")
-    else:
-        print ("[*]",rep)
-        Hitarät(Xa, Ya,rep , PGride)
-        #TillbackaVextning(Xa, Ya, TilpacaVext, MaxNertramp)
+    if FörsökUt == 1:
+        if rep == MaxF:
+            print("[X]")
+        else:
+            print ("[*]",rep)
+            Hitarät(Xa, Ya,rep , PGride)
 
 l = 0
 ValAvStartOchSlut = 0 
-while l < (Försök/4): 
+while l < (Försök/4) or l == (Försök/4): 
     for a in StartOchSlutStelen :
         X = a[0]
         Y = a[1]
         GoalX = a[2]
         GoalY = a[3]
-        new_func( Gride, X, Y, GoalX, GoalY,rep, MaxF,PGride, Xa, Ya)
+        PGride = [[1 for i in range(Ya)]for j in range(Xa)]
+        new_func( Gride, X, Y, GoalX, GoalY, rep, MaxF,PGride, Xa, Ya, FörsökUt)
         #PrintMap()
-        
+        #gameDisplay.fill(Black)
         TillbackaVextning(Xa, Ya, TilpacaVext, MaxNertramp)
-        SjärmUpdaterare(Xa, Ya, pixAr, GoalX, GoalY)
-    gameDisplay.fill(Black)
+        SjärmUpdaterare(Xa, Ya, pixAr, GoalX, GoalY,pixArP,VisaKArta,VisaVargeVäg)
+    if PintFörsök == 1:
+        print(l*4)
     l += 1
 print("Klar")
 gameDisplay.fill(Black)
-SjärmUpdaterare(Xa, Ya, pixAr, GoalX, GoalY)
-sleep(100)
+SjärmUpdaterare(Xa, Ya, pixAr, GoalX, GoalY,pixArP,VisaKArta,VisaVargeVäg)
+sleep(1000)
+
 
 
